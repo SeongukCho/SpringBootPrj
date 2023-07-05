7ad478b05bfe660265733a8f523ff36289d8b903
SeongukCho <tjddnr377@naver.com>
2023-07-05 10:29:00 +0900

    0705 스프링 로그인

--- src/main/java/kopo/poly/service/IUserInfoService.java
+++ src/main/java/kopo/poly/service/IUserInfoService.java
@@ -3,6 +3,7 @@ package kopo.poly.service;
 import kopo.poly.dto.UserInfoDTO;
 
 public interface IUserInfoService {
+
     UserInfoDTO getUserIdExists(UserInfoDTO pDTO) throws Exception;
 
     UserInfoDTO getEmailExists(UserInfoDTO pDTO) throws Exception;
@@ -11,7 +12,4 @@ public interface IUserInfoService {
 
     UserInfoDTO getLogin(UserInfoDTO pDTO) throws Exception;
 
-    UserInfoDTO searchUserIdOrPasswordProc(UserInfoDTO pDTO) throws Exception;
-
-    int newPasswordProc(UserInfoDTO pDTO) throws Exception;
 }
