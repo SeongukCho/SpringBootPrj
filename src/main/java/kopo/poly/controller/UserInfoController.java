93ac552181405b5bb7d111106a8cfb2a16bd71b0
SeongukCho <tjddnr377@naver.com>
2023-07-05 15:13:10 +0900

    0705 스프링 회원가입&로그인

--- src/main/java/kopo/poly/controller/UserInfoController.java
+++ src/main/java/kopo/poly/controller/UserInfoController.java
@@ -6,6 +6,7 @@ import kopo.poly.util.CmmUtil;
 import kopo.poly.util.EncryptUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,7 +21,7 @@ import javax.servlet.http.HttpSession;
 public class UserInfoController {
     private final IUserInfoService userInfoService;
 
-    /*회원가입 화면으로 이동*/
+    // 회원가입 화면으로 이동
     @GetMapping(value = "/user/userRegForm")
     public String userRegForm() {
         log.info(this.getClass().getName() + ".user/userRegForm");
@@ -28,10 +29,7 @@ public class UserInfoController {
         return "/user/userRegForm";
     }
 
-    /*
-     * 회원가입 로직 처리
-     *
-     */
+    // 회원가입 로직 처리
     @PostMapping(value = "/user/insertUserInfo")
     public String insertUserInfo(HttpServletRequest request, ModelMap modelMap) throws Exception {
         log.info(this.getClass().getName() + ".insertUserInfo start!");
@@ -44,9 +42,11 @@ public class UserInfoController {
 
         try {
             /*
+             * ########################################################
              * 웹 (회원정보 입력화면)에서 받는 정보를 String 변수에 저장 시작!!
              *
              * 무조건 웹으로 받은 정보는 pDTO에 저장하기 위해 임시로 String 변수에 저장함
+             * ########################################################
              */
             String user_id = CmmUtil.nvl(request.getParameter("user_id"));
             String user_name = CmmUtil.nvl(request.getParameter("user_name"));
@@ -56,6 +56,7 @@ public class UserInfoController {
             String addr2 = CmmUtil.nvl(request.getParameter("addr2"));
 
             /*
+             * ########################################################
              * 반드시, 값을 받았으면, 꼭 로그를 찍어서 값이 제대로 들어오는지 파악해야함
              * 반드시 작성할 것
              */
@@ -68,10 +69,12 @@ public class UserInfoController {
             log.info("addr2 : " + addr2);
 
             /*
+             * ########################################################
              * 웹(회원정보 입력화면)에서 받는 정보를 DTO에 저장하기 시작!!
              *
              * 무조건 웹으로 받은 정보는 DTO에 저장해야 한다고 이해하길 권함
-             * */
+             * ########################################################
+             */
 
             //웹(회원정보 입력화면)에서 받는 정보를 저장할 변수를 메모리에 올리기
             pDTO = new UserInfoDTO();
@@ -88,10 +91,12 @@ public class UserInfoController {
             pDTO.setAddr2(addr2);
 
             /*
-             * 웹(회원정보 입력화면)에서 받는 정보를 DTO에 저장하기 끝 !!
+             * ##################################################
+             *  웹(회원정보 입력화면)에서 받는 정보를 DTO에 저장하기 끝 !!
              *
              * 무조건 웹으로 받은 정보는 DTO에 저장해야 한다고 이해하길 권함
-             * */
+             * ##################################################
+             */
 
             // 회원가입
             res = userInfoService.insertUserInfo(pDTO);
@@ -100,20 +105,26 @@ public class UserInfoController {
 
             if (res == 1) {
                 msg = "회원가입되었습니다.";
-
+                url = "/main";
                 // 추후 회원가입 입력화면에서 ajax를 활용해서 아이디중복, 이메일중복을 체크하길 바람
-            } else if (res == 2) {
-                msg = "이미 가입된 아이디입니다.";
-
             } else {
                 msg = "오류로 인해 회원가입이 실패하였습니다.";
+                url = "/user/userRegForm";
             }
+        } catch (DuplicateKeyException e) { // PK인 USER_ID가 중복되어 에러가 발생했다면
+            msg = "이미 가입된 아이디입니다. 다른 아이디로 변경 후 다시 시도해주세요.";
+            url = "/user/userRegForm";
+            log.info(e.toString());
+            e.printStackTrace();
         } catch (Exception e) {
             // 저장이 실패하면 사용자에게 보여줄 메시지
-            msg = "실패하였습니다. : " + e;
+            msg = "시스템 오류로 실패하였습니다. 다시 시도해주세요.";
+            url = "/user/userRegForm";
             log.info(e.toString());
             e.printStackTrace();
         } finally {
+            log.info("출력할 메시지 : " + msg);
+            log.info("이동할 경로 : " + url);
             modelMap.addAttribute("msg", msg);
             modelMap.addAttribute("url", url);
 
@@ -125,13 +136,16 @@ public class UserInfoController {
 
     @PostMapping(value = "/user/loginProc")
     public String loginProc(HttpServletRequest request, ModelMap model, HttpSession session) {
+
         log.info(this.getClass().getName() + ".loginProc Start!");
 
         String msg = "";
         String url = "";
+
         UserInfoDTO pDTO = null;
 
         try {
+
             String user_id = CmmUtil.nvl(request.getParameter("user_id"));
             String password = CmmUtil.nvl(request.getParameter("password"));
 
@@ -141,6 +155,7 @@ public class UserInfoController {
             pDTO = new UserInfoDTO();
 
             pDTO.setUser_id(user_id);
+
 
             pDTO.setPassword(EncryptUtil.encHashSHA256(password));
 
@@ -156,8 +171,11 @@ public class UserInfoController {
                 session.setAttribute("SS_USER_ID", user_id);
                 session.setAttribute("SS_USER_NAME", CmmUtil.nvl(rDTO.getUser_name()));
 
-                msg = "로그인이 성공했습니다. \n" + rDTO.getUser_name() + "님 환영합니다.";
+                msg = "로그인에 성공했습니다. \n" + rDTO.getUser_name() + "님 환영합니다.";
                 url = "/main";
+            } else {
+                msg = "로그인에 실패하였습니다.";
+                url = "/user/login";
             }
         } catch (Exception e) {
             msg = "시스템 문제로 로그인이 실패했습니다.";
@@ -172,5 +190,18 @@ public class UserInfoController {
         }
 
         return "/redirect";
+    }
+
+    @GetMapping(value = "/user/login")
+    public String login() {
+        log.info(this.getClass().getName() + ".user/login Start!");
+        log.info(this.getClass().getName() + ".user/login End!");
+        return "/user/login";
+    }
+    @GetMapping(value = "/notice/noticeList")
+    public String noticeList(){
+        log.info(this.getClass().getName() + ".notice/noticeList Start!");
+        log.info(this.getClass().getName() + ".notice/noticeList End!");
+        return "/notice/noticeList";
     }
 }
